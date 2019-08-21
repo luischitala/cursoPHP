a0fee4d661d091c3a4cd7fce75b363326db49ee0
Luis Chitala <lrchitala@gmail.com>
2019-08-21 11:46:04 -0500

    Cookies

---
+++ cookies/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+setcookie('font-size','40px', time() - 60 * 60 * 24 * 30, '/' );
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Cookies</title>
+
+</head>
+<body>
+<h1>Cookie seteada</h1>
+</body>
+</html>
