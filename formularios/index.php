7c381a1e8f98e746772183d466b3cc09fb6d4f64
Luis Chitala <lrchitala@gmail.com>
2019-08-21 09:04:40 -0500

    Test

---
+++ formularios/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formularios</title>
+</head>
+<body>
+
+	<form action="" method="" name="" >
+		<input type="text" placeholder="Nombre">
+	</form>
+
+</body>
+</html>
