188e4bf06b4ebda05c8cac40f6e47ab0944d7da5
Luis Chitala <lrchitala@gmail.com>
2019-08-21 20:39:27 -0500

    Variables sanitized and hashed, working on login

--- practicas/login_registro/login.php
+++ practicas/login_registro/login.php
@@ -1,4 +1,17 @@
 <?php 
+
+if (isset($_SESSION['usuario'])) {
+	header('Location: index.php');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$usuario = filter_var(strtolower($_POST['usuario']),FILTER_SANITIZE_STRING);
+	$password = $_POST['password'];
+	$password = hash('sha512', $password);
+
+	echo "$usuario - $password";
+}
+
 require 'views/login.view.php';
 
 
