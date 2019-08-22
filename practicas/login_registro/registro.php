5fe9eb7cee384cdd9632dd8710f94386cf79051a
Luis Chitala <lrchitala@gmail.com>
2019-08-21 19:14:46 -0500

    Login with strings treated

--- practicas/login_registro/registro.php
+++ practicas/login_registro/registro.php
@@ -1,5 +1,15 @@
-<?php 
+<?php session_start();
 
-require 'views/registro.view.php'
+if (isset($_SESSION['usuario'])) {
+	header('Location: index.php');
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$usuario = filter_var (strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);
+	$password = $_POST['password'];
+	$password2 = $_POST['password2'];
+
+	echo "$usuario . $password . $password2";
+}
+require 'views/registro.view.php';
 
  ?>
--- practicas/login_registro/views/login.view.php
+++ practicas/login_registro/views/login.view.php
@@ -10,23 +10,21 @@
 </head>
 <body>
 	<div class="contenedor">
-		<h1 class="titulo">Registrate</h1>
+		<h1 class="titulo">Inicia sesión</h1>
 		<hr class="border">
 		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" class="formulario" name="login">
 			<div class="form-group">
 				<i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario">
 			</div>
+			
 			<div class="form-group">
-				<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña">
-			</div>
-			<div class="form-group">
-				<i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir contraseña">
+				<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña">
 				<i class="submit-btn fa fa-arrow-right"onclick="login.submit()"></i>
 			</div>
 		</form>
 		<p class="texto-registrate">
-			¿Ya tienes cuenta?
-			<a href="login.php">Iniciar sesión</a>
+			¿No tienes cuenta?
+			<a href="registro.php">Registrate</a>
 		</p>
 	</div>
 </body>
